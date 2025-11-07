0133cc13d922242f35ce75767025eb9dc5d885fd
JonyDevelops <jonyspahiu1000@gmail.com>
2025-11-07 15:59:28 +0000

    Added hamburger menu

--- JS/index.js
+++ JS/index.js
@@ -178,3 +178,18 @@ window.addEventListener('DOMContentLoaded', () => {
         renderCartItems();
     }
 });
+
+function togglemenu() {
+    const hamburger= document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.querySelector('.hamburger').classList.remove('active');
+            document.querySelector('nav').classList.remove('active');
+            });
+        }); 
