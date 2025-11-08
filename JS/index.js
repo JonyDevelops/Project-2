99338c9159e330f2e947f2202cbecd248c379596
JonyDevelops <jonyspahiu1000@gmail.com>
2025-11-08 22:30:50 +0000

    HTMl Validation fixed illegal spaces in img tags

--- JS/index.js
+++ JS/index.js
@@ -2,36 +2,37 @@
 let cart = [];
 
 const productData = {
+    // Hot Drinks
     'Cappuccino': { name: 'Cappuccino', price: 2.00, image: '/Assets/images/Cappuccino.jpg' },
     'black americano': { name: 'Black Americano', price: 2.00, image: '/Assets/images/black-americano.jpg' },
-    'Latte': { name: 'Latte', price: 2.00, image: '/Assets/images/Latte.png' },
+    'Latte': { name: 'Latte', price: 2.00, image: '/Assets/images/hot-latte.jpg' },
     'Flat white': { name: 'Flat White', price: 2.00, image: '/Assets/images/flat-white.png' },
     'Espresso': { name: 'Espresso', price: 2.00, image: '/Assets/images/Espresso.png' },
-    'Fliter black coffee': { name: 'Filter Black Coffee', price: 2.00, image: '/Assets/images/Fliter black coffee.jpeg' },
+    'Fliter black coffee': { name: 'Filter Black Coffee', price: 2.00, image: '/Assets/images/Fliter-black-coffee.jpeg' },
     'Macchiato': { name: 'Macchiato', price: 2.00, image: '/Assets/images/Macchiato.png' },
-    'Mocha': { name: 'Mocha', price: 2.00, image: '' },
+    'Mocha': { name: 'Mocha', price: 2.00, image: '/Assets/images/Mocha.jpg' },
 
     // Cold Drinks
     'Iced Latte': { name: 'Iced Latte', price: 2.50, image: '/Assets/images/Latte.png' },
     'Iced Americano': { name: 'Iced Americano', price: 2.50, image: '/Assets/images/iced-americano-sq.jpg' },
     'Iced Mocha': { name: 'Iced Mocha', price: 2.50, image: '/Assets/images/Iced_Cafe_Mocha.jpg' },
-    'Iced Espresso': { name: 'Iced Espresso', price: 2.50, image: '/Assets/images/Iced Espresso.jpeg' },
+    'Iced Espresso': { name: 'Iced Espresso', price: 2.50, image: '/Assets/images/Iced-Espresso.jpeg' },
     'Iced Macchiato': { name: 'Iced Macchiato', price: 2.50, image: '/Assets/images/IcedMacchiato.png' },
     'Iced Flat White': { name: 'Iced Flat White', price: 2.50, image: '/Assets/images/IcedFlatWhite.png' },
     'Frappe': { name: 'Frappe', price: 3.00, image: '/Assets/images/Fappe.jpg' },
-    'Caramel iced frappe': { name: 'Caramel Iced Frappe', price: 3.00, image: '/Assets/images/Caramal iced frappe.jpg' },
-    'Chocolate iced frappe': { name: 'Chocolate Iced Frappe', price: 3.00, image: '/Assets/images/Chocolate iced frappe.jpg' },
-    'Mocha Frappe': { name: 'Mocha Frappe', price: 3.00, image: '/Assets/images/Mocha Frappe.jpg' },
+    'Caramel iced frappe': { name: 'Caramel Iced Frappe', price: 3.00, image: '/Assets/images/Caramal-iced-frappe.jpg' },
+    'Chocolate iced frappe': { name: 'Chocolate Iced Frappe', price: 3.00, image: '/Assets/images/Chocolate-iced-frappe.jpg' },
+    'Mocha Frappe': { name: 'Mocha Frappe', price: 3.00, image: '/Assets/images/Mocha-Frappe.jpg' },
 
     // Snacks
-    'Chocolate croissant': { name: 'Chocolate croissant', price: 2.00, image: '/Assets/images/Chocolate croissant.jpg' },
+    'Chocolate croissant': { name: 'Chocolate croissant', price: 2.00, image: '/Assets/images/Chocolate-croissant.jpg' },
     'Croissant': { name: 'Croissant', price: 2.00, image: '/Assets/images/Croissant701SWSweb_2web.jpg' },
-    'McCoys Cheddar & Onion': { name: "McCoy's Cheddar & Onion", price: 2.00, image: '/Assets/images/Chedder & onion.jpg' },
+    'McCoys Cheddar & Onion': { name: "McCoy's Cheddar & Onion", price: 2.00, image: '/Assets/images/Chedder-and-onion.jpg' },
     'Chocolate Chip Cookie': { name: 'Chocolate Chip Cookie', price: 2.00, image: '/Assets/images/classic-chocolate-chip-cookies.jpg' },
-    'McCoys Salt & Vinegar': { name: "McCoys Salt & Vinegar", price: 2.00, image: "/Assets/images/McCoy's Salt & Vinegar.png" },
-    'White chocolate cookie': { name: 'White chocolate cookie', price: 2.00, image: '/Assets/images/White chocolate cookie.jpg' },
-    'Chocolate Donut': { name: 'Chocolate Donut', price: 2.00, image: '/Assets/images/Chocolate Donut.jpg' },
-    'Original glazed donut': { name: 'Original glazed donut', price: 2.00, image: '/Assets/images/original glazed donut.jpg' }
+    'McCoys Salt & Vinegar': { name: "McCoys Salt & Vinegar", price: 2.00, image: '/Assets/images/McCoys-Salt-and-Vinegar.png' },
+    'White chocolate cookie': { name: 'White chocolate cookie', price: 2.00, image: '/Assets/images/White-chocolate-cookie.jpg' },
+    'Chocolate Donut': { name: 'Chocolate Donut', price: 2.00, image: '/Assets/images/Chocolate-Donut.jpg' },
+    'Original glazed donut': { name: 'Original glazed donut', price: 2.00, image: '/Assets/images/original-glazed-donut.jpg' }
 };
 
 function addToCart(productName) {
@@ -192,4 +193,4 @@ const navLinks = document.querySelectorAll('nav a');
             document.querySelector('.hamburger').classList.remove('active');
             document.querySelector('nav').classList.remove('active');
             });
-        }); 
+        });
